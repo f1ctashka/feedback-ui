072911eadd2b2fd5511b741451a61bab4b4b637e
Вікторія Ільченко <v.ilchenko@kpi.ua>
2023-09-26 12:46:32 +0300

    created routes and links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import {useState} from "react";
+import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { v4 as uuidv4 } from 'uuid';
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackData from "./data/FeedbackData";
 import FeedbackForm from "./components/FeedbackForm";
+import AboutPage from "./pages/AboutPage";
+import AboutIconLink from "./components/AboutIconLink";
 
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
@@ -20,17 +23,27 @@ function App() {
     }
 
     return (
-        <>
+        <Router>
             <Header />
             <div className='container'>
-                <FeedbackForm handleAdd={addFeedback} />
-                <FeedbackStats feedback={feedback} />
-                <FeedbackList
-                    feedback={feedback}
-                    handleDelete={deleteFeedback}
-                />
+                <Routes>
+                    <Route exact path='/' element={
+                        <>
+                            <FeedbackForm handleAdd={addFeedback} />
+                            <FeedbackStats feedback={feedback} />
+                            <FeedbackList
+                                feedback={feedback}
+                                handleDelete={deleteFeedback}
+                            />
+                        </>
+                    }>
+
+                    </Route>
+                    <Route path='/about' element={<AboutPage />} />
+                </Routes>
+                <AboutIconLink />
             </div>
-        </>
+        </Router>
     )
 }
 
