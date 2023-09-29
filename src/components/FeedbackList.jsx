169ddf37fdc7eeadb01a695a136faa08c78d075d
Вікторія Ільченко <v.ilchenko@kpi.ua>
2023-09-29 10:52:22 +0300

    got global state with the useContext hook, added side effects with useEffect

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,8 +1,11 @@
 import { motion, AnimatePresence } from 'framer-motion';
+import { useContext } from "react";
 import FeedbackItem from "./FeedbackItem";
-import PropTypes from "prop-types";
+import FeedbackContext from "../context/FeedbackContext";
 
-function FeedbackList({ feedback, handleDelete }) {
+function FeedbackList() {
+    const {feedback} = useContext(FeedbackContext)
+
     if (!feedback || feedback.length === 0) {
         return <p>No feedback yet</p>
     }
@@ -19,22 +22,12 @@ function FeedbackList({ feedback, handleDelete }) {
                     <FeedbackItem
                         key={item.id}
                         item={item}
-                        handleDelete={handleDelete} />
+                    />
                 </motion.div>
             ))}
         </AnimatePresence>
 
     </div>
-}
-
-FeedbackItem.propTypes = {
-    feedback: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.number.isRequired,
-            text: PropTypes.string.isRequired,
-            rating: PropTypes.number.isRequired,
-        })
-    ),
 }
 
 export default FeedbackList;
